886f81e3bb76afb87c856b2242f82296b4222722
unknown <Kryaz2018@gmail.com>
2019-03-18 22:05:10 +0300

    Update css, service and controller

--- js/controllers/EventsController.js
+++ js/controllers/EventsController.js
@@ -1,9 +1,16 @@
 app.controller('eventsController',
     ['$scope', '$stateParams', 'EventsService', 'TicketsService',
     function($scope, $stateParams, EventsService, TicketsService) {
-        $scope.eventsFilter = EventsService.getEvents($stateParams.id).then(response => {
-            $scope.eventsFilter = response;
-            console.log(response, 123);
+        $scope.eventsFilter = EventsService.getEvents($stateParams.id).then(response =>
+        {
+            if( response != undefined ){
+                $scope.eventsFilter = response;
+            } else {
+                console.log('Sorry... Something went wrong.');
+                console.log(response);
+            }
+
+
         })
 
 
--- js/services/eventsService.js
+++ js/services/eventsService.js
@@ -8,62 +8,9 @@ app.factory('EventsService' , ['$http', function($http) {
                     return event.club == id;
                 })
                 return filteredEvents;
-            }, function(data) {
-                console.log(data);
+            }, function(error) {
+                console.log('Sorry, ' + error.statusText);
             })
-
     }
     return service;
 }])
-
-// app.factory('EventsService', ['$http', function($http) {
-//     function getEvents(clubId) {
-//         let events = [];
-//          $http.get('../data/events.json').then((response) => {
-//            return events = response.data;
-//         })
-//         return events;
-//     }
-//     return {
-//         getEvents: getEvents
-//     }
-// }])
-// app.factory('EventsService', ['$http', function($http) {
-//     let serviceEvents = {};
-//     let events = [];
-//     serviceEvents.getEventById = function(clubId) {
-//         $http.get('../data/events.json')
-//             .success(function(data) {
-//                 events = data;
-//                 let filteredEvents = events.filter((event) => {
-//                     return event.club == clubId;
-//                 })
-//                 return filteredEvents;
-//             })
-//
-//     }
-//     return serviceEvents;
-// }])
-// app.factory('EventsService', ['$http', function( $http) {
-//     let events = [];
-//     function getEvents() {
-//         $http.get('../data/events.json')
-//             .success(function(data) {
-//                 events = data;
-//             })
-//             .error(function(data) {
-//                 console.log(data);
-//             })
-//     }
-//     getEvents();
-//     let serviceEvents = {};
-//     serviceEvents.getEventById = function(clubId) {
-//         let filteredEvents = events.filter((event) => {
-//             return event.club == clubId;
-//         })
-//         return filteredEvents;
-//     }
-//     return serviceEvents;
-// }])
-
-
